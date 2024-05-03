b5d2bc7cb72fc4301221b1afd19ef90ed0141cc1
Manuel Alvarado <manuel.alvarado@bcr.gob.sv>
2024-05-03 09:14:30 -0600

    implement config-service in bootstrap.yml

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,11 +1,5 @@
-INSERT INTO tbl_invoices (id, number_invoice, description, customer_id, create_at, state) VALUES(1, '0001', 'invoice office items', 1, NOW(),'CREATED');
+INSERT INTO tbl_invoices (id, number_invoice, description, customer_id, create_at, state) VALUES(7, '0001', 'invoice office items', 1, NOW(),'CREATED');
 
-INSERT INTO tbl_invoice_items ( invoice_id, product_id, quantity, price ) VALUES(1, 1 , 1, 178.89);
-INSERT INTO tbl_invoice_items ( invoice_id, product_id, quantity, price)  VALUES(1, 2 , 2, 12.5);
-INSERT INTO tbl_invoice_items ( invoice_id, product_id, quantity, price)  VALUES(1, 3 , 1, 40.06);
-
-INSERT INTO tbl_invoices (id, number_invoice, description, customer_id, create_at, state) VALUES(2, '0002', 'invoice office items', 1, NOW(),'CREATED');
-
-INSERT INTO tbl_invoice_items ( invoice_id, product_id, quantity, price ) VALUES(2, 3 , 1, 178.89);
-INSERT INTO tbl_invoice_items ( invoice_id, product_id, quantity, price)  VALUES(2, 4 , 2, 12.5);
-INSERT INTO tbl_invoice_items ( invoice_id, product_id, quantity, price)  VALUES(2, 5 , 1, 40.06);
+INSERT INTO tbl_invoice_items ( invoice_id, product_id, quantity, price ) VALUES(7, 1 , 1, 178.89);
+INSERT INTO tbl_invoice_items ( invoice_id, product_id, quantity, price)  VALUES(7, 2 , 2, 12.5);
+INSERT INTO tbl_invoice_items ( invoice_id, product_id, quantity, price)  VALUES(7, 3 , 1, 40.06);
